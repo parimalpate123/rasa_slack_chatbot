77e0dfa9917f5dffac6e78f034fa97e9dd83cfb0
Parimal Patel <parimalpatel123@gmail.com>
2019-02-18 19:43:27 +0530

    Add files via upload

--- actions.py
+++ actions.py
@@ -1,12 +1,83 @@
 from rasa_core.actions import Action
+from rasa_core_sdk.events import SlotSet
+import httplib2
 
 
-class ActionExample(Action):
+
+class ActionCheckService(Action):
 
     def name(self):
         # you can then use action_example in your stories
-        return "action_example"
+        return "action_checkservice"
 
     def run(self, dispatcher, tracker, domain):
         # what your action should do
+        s=str(tracker.get_slot("servicename"))
+        URL="https://www."+s+".com"
+        try:
+            resp, content = httplib2.Http().request(URL)
+            if resp.status==200:
+                response="""{} is up and running  """.format(URL)
+                dispatcher.utter_message(response)
+            elif resp.status==302:
+                response="""{} is up and running  """.format(URL)
+                dispatcher.utter_message(response)
+            else:
+                dispatcher.utter_message("Errorcode :"+str(resp.status)+" For website:"+URL)
+        except:
+            dispatcher.utter_message("Sorry! an exception occured in accessing "+s+" website")
         return []
+
+
+class ActionStartJenkinsBuild(Action):
+
+    def name(self):
+        # you can then use action_example in your stories
+        return "action_jenkins"
+
+    def run(self, dispatcher, tracker, domain):
+        # what your action should do
+        job=str(tracker.get_slot('jenkinsjob'))
+        #job = str(tracker.get_slot('jenkinsjob'))
+        URL='http://localhost:8080/job/' + job \
+            + '/build?token=remote_enable_token'
+        dispatcher.utter_message('URL :' + URL)
+        try:
+            (resp, content) = httplib2.Http().request(URL)
+            if resp.status == 201:
+                response = 'Job successfully triggered'
+                dispatcher.utter_message(response)
+                dispatcher.utter_message(str(content.decode('utf-8')))
+            else:
+                dispatcher.utter_message('status code :'
+                        + str(resp.status))
+                dispatcher.utter_message(content.decode())
+        except:
+            dispatcher.utter_message('Sorry! an exception occured in triggering the job'
+                    )
+        return []
+
+
+        
+class ActionStartJenkinsBuildWithParams(Action):
+    def name(self):
+        return "action_jenkins_param"
+
+    def run(self, dispatcher, tracker, domain):
+        job = str(tracker.get_slot("appname"))
+#        if(tracker.get_slot("param")=="environment"):
+#            param = "Environment"
+        pvalue = str(tracker.get_slot("environment")).upper()
+        URL = "http://localhost:8080/job/"+job+"/buildWithParameters?token=remote_enable_token&Environment="+pvalue
+        dispatcher.utter_message("URL :" + URL)
+        try:
+            resp, content = httplib2.Http().request(URL)
+            if resp.status==201:
+                response="Job successfully triggered with "+param+"="+pvalue
+                dispatcher.utter_message(response)
+            else:
+                dispatcher.utter_message("status code :"+str(resp.status))
+                dispatcher.utter_message(content.decode())
+        except:
+            dispatcher.utter_message("Sorry! an exception occured in triggering the job")
+        return []        
--- run_app.py
+++ run_app.py
@@ -7,8 +7,8 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/current/nlu')
 agent = Agent.load('./models/current/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-525465834114-524864270065-525687865621-959ed2b6ed9f82502442ed44d0cffe89', #app verification token
-							'xoxb-525465834114-525382855891-SYt6HyWl7IfVyhtX19z6jJec', # bot verification token
+input_channel = SlackInput('xoxp-525465834114-524864270065-550012447110-1827c49d338679fe1e329374f61ee6af', #app verification token
+							'xoxb-525465834114-525382855891-9wJ3kN1DTJ9Plp6r1nXt6e9v', # bot verification token
 							'N7oZdLwzfCZ3sm1syUsTn0Nl', # slack verification token
 							True)
 
