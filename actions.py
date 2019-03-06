a6d111d2e1f8411f59c210287c3f50fc111ccfdd
priyankagupta17 <47778730+priyankagupta17@users.noreply.github.com>
2019-03-06 21:51:06 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,4 +1,10 @@
 from rasa_core.actions import Action
+from rasa_core.actions.forms import (
+    BooleanFormField,
+    EntityFormField,
+    FormAction,
+    FreeTextFormField
+)
 from rasa_core_sdk.events import SlotSet
 import httplib2
 import requests
@@ -8,31 +14,27 @@ password='Accenture@123'
 headers={'Content-Type':'application/json'}
 resp=[]
 temp= list()
-samplejson={
-        "fields":
-                {
-                        "project":
-                                {
-                                        "key": "TS"
-                                },
-                        "summary": "Creating of an issue using project keys and issue type names using the REST API",
-                        "description": "Creating of an issue using project keys and issue type names using the REST API",
-                        "issuetype":
-                                {
-                                        "name": "Task"
-                                }
-                }
-}
+summary=""
+description=""
 
 
 
-class ActionCheckService(Action):
+
+class ActionCheckService(FormAction):
+
+	RANDOMIZE = False
+
+	@staticmethod
+	def required_fields():
+		return [
+			EntityFormField("servicename", "servicename")
+			]
 
 	def name(self):
 		# you can then use action_example in your stories
 		return "action_checkservice"
 
-	def run(self, dispatcher, tracker, domain):
+	def submit(self, dispatcher, tracker, domain):
 		# what your action should do
 		s=str(tracker.get_slot("servicename"))
 		URL="https://www."+s+".com"
@@ -51,16 +53,20 @@ class ActionCheckService(Action):
 		return []
 
 
-class ActionStartJenkinsBuild(Action):
+class ActionStartJenkinsBuild(FormAction):
+	RANDOMIZE = False
+	@staticmethod
+	def required_fields():
+		return [
+			EntityFormField("jenkinsjob", "jenkinsjob")
+			]
 	def name(self):
 		# you can then use action_example in your stories
 		return "action_jenkins"
 
-	def run(self, dispatcher, tracker, domain):
+	def submit(self, dispatcher, tracker, domain):
 		# what your action should do
-		#job=str(tracker.get_slot('jenkinsjob'))
-		dispatcher.utter_message('Enter name of jenkins App :')
-		job=input()
+		job=str(tracker.get_slot('jenkinsjob'))
 		URL='http://localhost:8080/job/' + job + '/build?token=remote_enable_token'
 		dispatcher.utter_message('URL :' + URL)
 		try:
@@ -80,21 +86,32 @@ class ActionStartJenkinsBuild(Action):
 
 
 		
-class ActionStartJenkinsBuildWithParams(Action):
+class ActionStartJenkinsBuildWithParams(FormAction):
+	RANDOMIZE = False
+	@staticmethod
+	def required_fields():
+		return [
+			EntityFormField("appname", "appname"),
+			EntityFormField("environment", "environment")
+			]
 	def name(self):
 		return "action_jenkins_param"
 
-	def run(self, dispatcher, tracker, domain):
+	def submit(self, dispatcher, tracker, domain):
 		job = str(tracker.get_slot("appname"))
 #               if(tracker.get_slot("param")=="environment"):
 #               param = "Environment"
 		pvalue = str(tracker.get_slot("environment")).upper()
 		URL = "http://localhost:8080/job/"+job+"/buildWithParameters?token=remote_enable_token&Environment="+pvalue
-		dispatcher.utter_message("URL :" + URL)
+		if pvalue == "UAT" or pvalue == "INT" or pvalue == "DEV":
+			dispatcher.utter_message("Access : You have the required access for this job" )
+		if pvalue == "PROD":
+			dispatcher.utter_message("Access : You do not have the required access for this job, please contact Release Management Team" )
+			return[]
 		try:
 			resp, content = httplib2.Http().request(URL)
 			if resp.status==201:
-				response="Job successfully triggered with "+param+"="+pvalue
+				response=job+" Job successfully triggered in "+pvalue
 				dispatcher.utter_message(response)
 			else:
 				dispatcher.utter_message("status code :"+str(resp.status))
@@ -103,11 +120,17 @@ class ActionStartJenkinsBuildWithParams(Action):
 			dispatcher.utter_message("Sorry! an exception occured in triggering the job")
 		return []        
 
-class ActionGetJIRAStatus(Action):
+class ActionGetJIRAStatus(FormAction):
+	RANDOMIZE = False
+	@staticmethod
+	def required_fields():
+		return [
+			EntityFormField("JIRAID", "JIRAID")
+			]
 	def name(self):
 		return "action_GetJIRAStatus_param"
 
-	def run(self, dispatcher, tracker, domain):
+	def submit(self, dispatcher, tracker, domain):
 		ID = str(tracker.get_slot("JIRAID"))
 		URL = "https://parimalpatel123.atlassian.net/rest/api/2/issue/"+ID+"?fields=status"
 		#dispatcher.utter_message("URL :" + URL)
@@ -127,13 +150,36 @@ class ActionGetJIRAStatus(Action):
 			dispatcher.utter_message(resp.text)
 			dispatcher.utter_message(resp.status_code)
 		return []
-	
-class ActionGetJIRACreate(Action):
+
+class ActionGetJIRACreate(FormAction):
+	RANDOMIZE = False
+	@staticmethod
+	def required_fields():
+		return [
+			EntityFormField("summary", "summary"),
+			EntityFormField("description", "description")
+			]
 	def name(self):
 		return "action_createJIRArequest_param"
 
-	def run(self, dispatcher, tracker, domain):
-		#ID = str(tracker.get_slot("JIRAID"))
+	def submit(self, dispatcher, tracker, domain):
+		summary = str(tracker.get_slot("summary"))
+		description = str(tracker.get_slot("description"))
+		samplejson={
+				"fields":
+					{
+			"project":
+							{
+								"key": "TS"
+							},
+						"summary": summary,
+						"description": description,
+						"issuetype":
+							{
+								"name": "Task"
+							}
+					}
+		}
 		URL = "https://parimalpatel123.atlassian.net/rest/api/2/issue/"
 		#dispatcher.utter_message("URL :" + URL)
 		try:
@@ -141,7 +187,30 @@ class ActionGetJIRACreate(Action):
 			a=resp.json()
 			dispatcher.utter_message("New Issue is created with ID : "+a['key'])	
 		except:
-			dispatcher.utter_message("exception")
+			dispatcher.utter_message(resp.text)
 			#dispatcher.utter_message(resp.status_code)
 		return []
 	
+
+
+class ActionSearchRestaurants(FormAction):
+
+    RANDOMIZE = False
+
+    @staticmethod
+    def required_fields():
+	    return [
+		    EntityFormField("cuisine", "cuisine"),
+		    EntityFormField("number", "people")
+		    ]
+
+    def name(self):
+	    return 'action_search_restaurants'
+
+    def submit(self, dispatcher, tracker, domain):
+	    results = RestaurantAPI().search(
+		    tracker.get_slot("cuisine"),
+		    tracker.get_slot("people"))
+	    return [SlotSet("search_results", results)]
+
+
